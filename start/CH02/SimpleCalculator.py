1f969beeba30dde3f342533c62a80296d5191c27
GR-Eliki-Smith <smith.eric@student.greenriver.edu>
2024-04-11 09:47:19 -0700

    Created the Simple Calculator script

--- start/CH02/SimpleCalculator.py
+++ start/CH02/SimpleCalculator.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 # A simple calculator to show math and conditionals
-# Created 
+# Created 4/11/24 by Eric Smith
+
+# ask user for two numbers
+num_a = int( input("What is the first number? ") )
+num_b = int( input("What is the second number? ") )
+
+# Add/Substract/Multiply/Divide the numbers
+print (num_a + num_b)
