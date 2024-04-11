f531f223b7b9384fe4c5cde86b806a7477e5530a
GR-Eliki-Smith <smith.eric@student.greenriver.edu>
2024-04-11 09:27:53 -0700

    Created Hello World 3 script

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
-# Created 
+# Created 4/11/24
+
+# Ask the user for their name
+user_name = input("What is your name? ")
+
+# Say hello to the user
+print("Hello", user_name)
+print("Hello {0})".format(user_name))
+print(f"Hello {user_name}")
+print("Hello" + user_name)
+message = "Hello" + user_name
+print(message)
