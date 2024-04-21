8307fac52a7f5183e9471e3349fad9e281d7da15
GR-Eliki-Smith <smith.eric@student.greenriver.edu>
2024-04-21 16:14:09 -0700

    Added Loop script

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Eliki Smith - 4/21/24
+
+response = input("Is today a good day? (y/n) ")
+if response.lower()=='y':
+    for i in range(10):
+        print("Yeah it is")
