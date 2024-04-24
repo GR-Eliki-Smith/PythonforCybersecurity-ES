9b3f506c0da4ecce847888aae3c474c73eb53253
GR-Eliki-Smith <smith.eric@student.greenriver.edu>
2024-04-24 14:41:35 -0700

    Added Readfile script

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Eliki Smith 4/24/24
+
+file = open('hackme.txt','r')
+print("Here is someone to hack - information")
+for line in file:
+    print(line)
+
+file.close()
