422a88bc96d9648f26b204ad08126a7e7d9ccb03
GR-Eliki-Smith <smith.eric@student.greenriver.edu>
2024-04-30 09:48:19 -0700

    ROT13 script (almost) finished.

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By Eliki Smith 4/30/24
+
+#Ask for Message
+message = input("What is the message? ")
+message = message.lower()
+#Foreach Letter
+for letter in message:
+    print(letter)
+    #change to number
+    letter_number = ord (letter)
+    #is this a letter?
+    if letter_number >= 97 and letter_number <= 122:
+        #Add 13
+        letter_number = letter_number + 13
+        #GTR than 26?
+        if letter_number > 122:
+            #Sub 26
+            letter_number -= 26
+    #Change to letter
+    print(letter_number)
+# Print message
+#print(new_message)
