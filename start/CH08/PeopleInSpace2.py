387147933c70c543479b5bd94dcd59b3ab8f848a
GR-Eliki-Smith <smith.eric@student.greenriver.edu>
2024-05-21 09:48:41 -0700

    Added people in space scripts

--- start/CH08/PeopleInSpace2.py
+++ start/CH08/PeopleInSpace2.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Script that tells us how many people there are in space
-#By 
+#By Eliki Smith - 05/21/24
+
+#Import things
+import requests
+
+#get people in space function
+def get_people_in_space():
+    url = "http://api.open-notify.org/astros.json"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    items = response.json()
+    return items
+
+#Print basic details
+astronauts = get_people_in_space()
+print(astronauts)
+
+#Print only the number of people in space
+ast_num = astronauts["number"]
+print ("There are currently {0} people in space".format( ast_num ))
