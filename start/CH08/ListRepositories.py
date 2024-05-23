126a0495361714cb161cfae9a29cd4197ddc9dc2
GR-Eliki-Smith <smith.eric@student.greenriver.edu>
2024-05-23 09:46:41 -0700

    List Repositories scripts

--- start/CH08/ListRepositories.py
+++ start/CH08/ListRepositories.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 # Script that lists repositories in GitHub
 # Requires a Personal Access Token to run
-# By 
+# By Eliki Smith - 05/23/24
+
+import requests
+import configparser
+
+def get_api_key(key_name):
+    config = configparser.ConfigParser()
+    config.read('/home/justincase/secrets.ini')
+    api_key = config["APIKeys"][key_name]
+    return api_key
+
+token = get_api_key("github")
+
+url = "https://api.github.com/user/repos"
+
+payload={}
+headers={
+    'Accept': 'application/json',
+    'Authorization': 'Bearer' + token
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
